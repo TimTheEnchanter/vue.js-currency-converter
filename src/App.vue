861e8d971379dc092eca3c9158d778ee38f8b29d
James Parrott <japarrott@shaw.ca>
2024-11-14 14:54:13 -0700

    initial build, add form elements

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <main>
+    <input type="number" :value="" @change="usdToCAD"> USD
+    <input type="number" :value="" @change="cadToUSD"> CAD
+  </main>
+
+</template>
