08036858816ede7be52f7c3e61c78413156b4cd6
James Parrott <japarrott@shaw.ca>
2024-11-14 21:50:41 -0700

    build out script

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <script setup>
+import { ref } from 'vue'
 
+const uToC = ref(0)
+const cToU = ref(0)
+
+function usdToCAD(e, v = +e.target.value) {
+  uToC.value = v
+  cToU.value = //Need to add formula
+}
+
+function cadToUSD(e, v = +e.target.value) {
+  cToU.value = v
+  uToC.value = //Need to add formula
+}
 </script>
 
 <template>
 
   <main>
-    <input type="number" :value="" @change="usdToCAD"> USD
-    <input type="number" :value="" @change="cadToUSD"> CAD
+    <input type="number" :value="uToC" @change="usdToCAD"> USD
+    <input type="number" :value="cToU" @change="cadToUSD"> CAD
   </main>
 
 </template>
